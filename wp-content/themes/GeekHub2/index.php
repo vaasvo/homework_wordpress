0068601adcd54c18af553941363bb4e94bfaa8a3
vaasvo <programix@nm.ru>
2014-04-01 08:50:50 +0300

    deleted paragraph description GeekHub Inserted GeekHub using the page_id

--- wp-content/themes/GeekHub2/index.php
+++ wp-content/themes/GeekHub2/index.php
@@ -1,22 +1,25 @@
 <?php get_header(); ?>
 		<div id="content">
-			<p><strong>GeekHub</strong> — це проект, що надає можливість отримати практичні знання та навички в сфері розробки програмного забезпечення. На відміну від традиційної освіти, викладачі GeekHub працюють з новітніми технологіями у провідних софтверних компаніях, тому слухачі GeekHub отримують тільки актуальні знання. Якщо ти зацікавлений — запрошуємо ознайомитись з деталями проекту, та <a href="#">зареєструватися</a> слухачем!</p>
+<?php query_posts('page_id=2'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php the_content(); ?>
+<?php endwhile; endif; ?>
+
 			<h3>Наші курси</h3>
 			<ul>
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php $courses = new WP_Query(array('post_type' => 'courses',
+										'orderby' => 'ID',
+										'order'=> 'ASC'
+							)); ?>
+<?php if ( $courses->have_posts() ) : while ( $courses->have_posts() ) : $courses->the_post(); ?>
 
 				<li id="post-<?php the_ID(); ?>">
 <?php the_post_thumbnail(); ?>
 					<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 <?php the_content();?>
 				</li>
-
-
 <?php endwhile; ?>
-
 <?php else: ?>
-<!-- no posts found -->
+<p>post area</p>
 <?php endif; ?>
 			</ul>
 		</div>
