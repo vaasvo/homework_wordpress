ff49b02dc08c0f55bcaa6f39a4599086d64e6a27
vaasvo <programix@nm.ru>
2014-04-06 22:00:24 +0300

    delete loop

--- wp-content/themes/GeekHub2/page.php
+++ wp-content/themes/GeekHub2/page.php
@@ -1,21 +1,6 @@
 <?php get_header(); ?>
 	<div id="content">
 		<?php the_title('<h3>', '</h3>'); ?>
-		<ul>
-			<?php query_posts(array('orderby' => 'ID', 'order'=> 'ASC')); ?>
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-			<li>
-				<h4><span></span></h4>
-				<?php the_post_thumbnail(); ?>
-				<?php the_content();?>
-			</li>
-
-			<?php endwhile; ?>
-			<?php else: ?>
-			<!-- no posts found -->
-			<?php endif; wp_reset_query(); ?>
-			</ul>
+		<?php the_content();?>
 	</div>
-
 <?php get_footer(); ?>
